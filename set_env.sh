fb9bc20d5f85218d5ab4d663743692d664372419
Nico <nphilibert17@gmail.com>
2022-12-28 11:25:46 +0300

    firt commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=udagramdev
+export POSTGRES_HOST=udagramdev.cpghhqizttsm.eu-west-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdev
+export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_REGION=eu-west-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
